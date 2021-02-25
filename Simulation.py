5f2bc00c1472132629d28f99c725699b35ff00b4
Shaun Plummer <Shaunplummer247@googlemail.com>
2021-02-25 20:11:07 +0000

    feat: initial working implementation

--- Simulation.py
+++ Simulation.py
@@ -11,18 +11,21 @@ class Simulation:
 
     def run(self):
         print("Running simulation")
+        self.writeToFile()
 
     def writeToFile(self):
-        with open("output" + self.file_name(), "a") as file:
+        with open("output/" + self.file_name, "w+") as file:
             # Print the total number of intersections
-            file.write(str(len(self.environment.intersections)))
-
+            intersections = self.environment.intersections
+            file.write(str(len(intersections)) + "\n")
             # iterate the intersections
-            for inter in self.environment.intersections:
-                # print the number of incomming streets
-                file.write(str(len(inter.street_in)))
-                # iterate the streets and print their time
-                for street in inter.street_in:
-                    file.write(str(street.time))
+            for inter in intersections:
+                print("\n Intersection" + str(inter.num) + " in: " + str(inter.streets_in))
+                # print the number of incoming streets
+                file.write(str(inter.num) + "\n")
+                file.write(str(len(inter.streets_in)) + "\n")
+                # iterate the streets and
+                for street_name in inter.streets_in:
+                    street_model = self.environment.getStreet(street_name)
 
-        file.close()
+                    file.write(street_model.name + " " + str(street_model.time) + " \n")
