dba0d3b56024790884b6a29cdfb9685227e8b3bc
Shaun Plummer <Shaunplummer247@googlemail.com>
2021-02-25 19:34:23 +0000

    feat: add output logic

--- Simulation.py
+++ Simulation.py
@@ -12,9 +12,17 @@ class Simulation:
     def run(self):
         print("Running simulation")
 
-        for int in self.environment.intersection:
-            int.getMaxStreetWithMaxCars()
+    def writeToFile(self):
+        with open("output" + self.file_name(), "a") as file:
+            # Print the total number of intersections
+            file.write(str(len(self.environment.intersections)))
 
+            # iterate the intersections
+            for inter in self.environment.intersections:
+                # print the number of incomming streets
+                file.write(str(len(inter.street_in)))
+                # iterate the streets and print their time
+                for street in inter.street_in:
+                    file.write(str(street.time))
 
-
-
+        file.close()
