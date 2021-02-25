1161db4092733cdecbb85787ce97286c14b13341
BillyHaggar <47534177+BillyHaggar@users.noreply.github.com>
2021-02-25 18:29:49 +0000

    feat: v-line parse

--- Models.py
+++ Models.py
@@ -1,16 +1,14 @@
 
-
 class Intersection:
-    def __init__(self, num):
+    def __init__(self, num, street):
         self.__num = num
+        self.__steets = street
 
 class Street:
     def __init__(self, name, start, end):
         self.__name = name
         self.__start = start
         self.__end = end
-
-
 
 
 
--- Parser.py
+++ Parser.py
@@ -1,10 +1,21 @@
 from InputData import Environment
-
+from Models import *
 
 def process_input(file_path):
     file = open(file_path)
-
     header = file.readline()
     print("Processing " + file_path + " with header " + header)
 
-    return Environment(0, 0, 0)
+    header_array = header.split()
+    print(header_array)
+
+    with open(file_path) as fp:
+        for cnt, line in enumerate(fp):
+            if cnt > int(header_array[2]):
+                streets = line.split()
+                streets.pop(0)
+                print(streets)
+        
+
+
+    return Environment(header_array[0], header_array[1], header_array[2], header_array[3], header_array[4])
