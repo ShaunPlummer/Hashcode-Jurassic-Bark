5d5980e8cd5781a03e316e4a7d538476f68b9d3c
Chris <kpjanusiewicz@gmail.com>
2021-02-25 21:01:40 +0000

    feat: base time on weighing

--- Simulation.py
+++ Simulation.py
@@ -14,6 +14,8 @@ class Simulation:
         self.writeToFile()
 
     def writeToFile(self):
+        baseline = 30
+
         with open("output/" + self.file_name, "w+") as file:
             # Print the total number of intersections
             intersections = self.environment.intersections
@@ -22,10 +24,21 @@ class Simulation:
             for inter in intersections:
                 # print("\n Intersection" + str(inter.num) + " in: " + str(inter.streets_in))
                 # print the number of incoming streets
+
+                traffic_count_all = sum(self.environment.streetList[s].traffic_count for s in inter.streets_in)
+
                 file.write(str(inter.num) + "\n")
                 file.write(str(len(inter.streets_in)) + "\n")
                 # iterate the streets and
                 for street_name in inter.streets_in:
                     street_model = self.environment.streetList[street_name]
 
-                    file.write(street_model.name + " " + str(street_model.time) + " \n")
+                    traffic_count = self.environment.streetList[street_name].traffic_count
+
+                    if traffic_count_all == 0:
+                        time = 0
+                    else:
+                        time = int(traffic_count / traffic_count_all) * baseline
+
+                    if time != 0:
+                        file.write(street_model.name + " " + str(time) + " \n")
